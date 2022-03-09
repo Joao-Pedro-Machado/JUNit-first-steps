4ac42fc04deb7880867febdd2f27a9c7be49bb51
João Pedro Machado <joao.machado0601@gmail.com>
2022-03-09 14:35:57 -0300

    Creating BeforeEach initialize method for test class

--- src/test/java/br/com/alura/tdd/service/BonusServiceTest.java
+++ src/test/java/br/com/alura/tdd/service/BonusServiceTest.java
@@ -1,6 +1,7 @@
 package br.com.alura.tdd.service;
 
 import br.com.alura.tdd.modelo.Funcionario;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 import java.math.BigDecimal;
@@ -12,9 +13,15 @@ import static org.junit.jupiter.api.Assertions.assertThrows;
 
 public class BonusServiceTest {
 
+    private BonusService service;
+
+    @BeforeEach
+    public void inicializar() {
+        service = new BonusService();
+    }
+
     @Test
     void bonusDeveriaSerZeroParaFuncionarioComSalarioMuitoAlto() {
-        BonusService service = new BonusService();
         assertThrows(
                 IllegalArgumentException.class,
                 () -> service.calcularBonus(new Funcionario("João", LocalDate.now(), new BigDecimal("25000")))
@@ -23,17 +30,13 @@ public class BonusServiceTest {
 
     @Test
     void bonusDeveriaSer10PorCentoDoSalario() {
-        BonusService service = new BonusService();
         BigDecimal bonus = service.calcularBonus(new Funcionario("João", LocalDate.now(), new BigDecimal("2500")));
-
         assertEquals(new BigDecimal("250.00"), bonus);
     }
 
     @Test
     void bonusDeveriaSer10PorCentoParaSalarioDeExatamente10000() {
-        BonusService service = new BonusService();
         BigDecimal bonus = service.calcularBonus(new Funcionario("João", LocalDate.now(), new BigDecimal("10000")));
-
         assertEquals(new BigDecimal("1000.00"), bonus);
     }
 
